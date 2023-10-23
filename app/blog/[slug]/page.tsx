5b77490e2fd53158bd93fa4171bf8a4ba1a77fb8
HoanLV <tuibui1111@gmail.com>
2023-10-23 21:27:14 +0700

    pass test

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -10,7 +10,7 @@ export async function generateMetadata({ params }: Params) {
 
 export default function Page({ params }: Params) {
   return <><h1>Slug: {params.slug}</h1>
-  <p>HoanLV wellcome to cicd github</p>
+  <p>HoanLV</p>
   <p>change code</p>
   </>
 }
